8fc57313edb1080fdca6dcf382d53d09b3b2fdfa
Ufuk Kirmizigedik <67425232+ufukkirmizigedik@users.noreply.github.com>
2025-06-01 18:52:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ client_id = "your client ID"
 client_secret = "secret key"
 redirect_uri = "http://localhost:8000/callback"
 
-# Kullanıcıyı yetki sayfasına yönlendir
+
 auth_url = f"https://hh.ru/oauth/authorize?response_type=code&client_id={client_id}&state=abc123"
 print("Tarayıcıda açılıyor:", auth_url)
 webbrowser.open(auth_url)
 
-# Kullanıcı giriş yapıp izin verince, URL'den 'code=' parametresini alacağız
-authorization_code = input("Tarayıcıdan geri dönen URL'deki code değerini yapıştırın: ")
+
+authorization_code = input("Paste the 'code' parameter from the URL after authorization in your browser: ")
 
 # Token alma isteği
 data = {
